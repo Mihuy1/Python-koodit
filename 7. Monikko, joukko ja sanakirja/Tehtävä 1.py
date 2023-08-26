69303c0c3cee64b1a253f2699674c27d1f2658c8
Mihuy1 <patrik.hyytiainen@protonmail.com>
2023-08-26 10:13:04 +0300

    Moduuli 7. Tehtävä lisätty

--- 7. Monikko, joukko ja sanakirja/Tehtävä 1.py
+++ 7. Monikko, joukko ja sanakirja/Tehtävä 1.py
@@ -1,7 +1,12 @@
+vuodenajat = {1: "talvi", 2: "talvi", 3: "talvi", 4: "kevät", 5: "kevät", 6: "kevät", 7: "kesä",
+              8: "kesä", 9: "kesä", 10: "syksy", 11: "syksy", 12: "talvi"}
 
-vuodenajat = ("kevät", "syksy", "talvi")
 
-userInput = input("Anna kuukauden numero: ")
+kuukausiNumero = int(input("Anna kuukauden numero: "))
+
+if kuukausiNumero in vuodenajat:
+    vuodenaika = vuodenajat[kuukausiNumero]
+    print(f"{kuukausiNumero}. kuukausi on {vuodenaika}")
 
 
 
